250be73197c16fccc6457a8ecc021419f53b5cd5
Darian Patzak <darian.patzak@mailbox.tu-dresden.de>
2022-07-29 01:54:24 +0200

    moved history update last in the update order to prevent dpendencie issues

--- update_database.py
+++ update_database.py
@@ -2,8 +2,8 @@ from recently import RecentSongs
 
 
 songs = RecentSongs()
-songs.update_history_with_similar_artist_name()
 songs.update_song_database_with_history_data()
 songs.update_artists_with_similar_known_names()
 songs.update_song_database_with_audio_features()
 songs.update_song_database_with_artists()
+songs.update_history_with_similar_artist_name()
