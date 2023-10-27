afdc580354129d3d600fd80c07450924eaad1700
theryann <darianpatzak@web.de>
2023-10-27 13:10:17 +0200

    feat new sql analysis script

---
+++ stats.sql
@@ -0,0 +1,27 @@
+SELECT * 
+FROM
+    ( SELECT count(*) as '#streams' FROM Stream ),
+    ( SELECT count(*) as '#artists' FROM Artist ),
+    ( SELECT count(*) as '#songs'   FROM Song ),
+    ( SELECT 
+        CAST (( SELECT count(*) FROM Album WHERE imgBigLocal IS NULL ) * 1.0
+        / count(*)
+        * 100
+        as INT)
+        as '% Album img local'          
+      FROM Album
+     ),   
+    ( SELECT 
+        CAST (( SELECT count(*) FROM Artist WHERE imgBigLocal IS NULL ) * 1.0
+        / count(*)
+        * 100
+        as INT)
+        as '% Artists img local'          
+      FROM Artist
+     )   
+
+
+
+
+
+
